c9698c1e5096fbd51679173815438971999eea36
GaddamS <saikumar.gaddam@proagrica.com>
2023-10-16 16:45:40 +0530

    created dto classess

---
+++ src/main/java/com/easybank/accounts/dto/AccountsDto.java
@@ -0,0 +1,10 @@
+package com.easybank.accounts.dto;
+
+import lombok.Data;
+
+@Data
+public class AccountsDto {
+    private Long accountNumber;
+    private String accountType;
+    private String mobileNumber;
+}
